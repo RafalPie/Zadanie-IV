fa950d1e5fcd36d2ab9896551b1b3b5282dc529d
Rafał Piechna <rpiechna@paradyz.com.pl>
2022-07-10 16:04:27 +0200

    deklaracja zmiennych

--- ZadanieIV/zadanieIV.py
+++ ZadanieIV/zadanieIV.py
@@ -9,3 +9,12 @@ rozmiar_pierwszego_pliku=os.path.getsize(nazwa_pierwszego_pliku)
 rozmiar_drugiego_pliku=os.path.getsize(nazwa_drugiego_pliku)
 print(str(rozmiar_pierwszego_pliku)+ 'to jest drugi plik :' + str(rozmiar_drugiego_pliku))
 snrindB_range = range(0, 10)
+itr = len(snrindB_range)
+N = 100000
+ber = [None]*itr
+ber1 = [None]*itr
+tx_symbol = 0
+noise = 0 
+ch_coeff = 0
+rx_symbol = 0 
+det_symbol = 0
