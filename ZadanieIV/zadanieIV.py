496058252989b59f5dfce071087aa1e8de5ce962
Rafał Piechna <rpiechna@paradyz.com.pl>
2022-07-10 16:05:03 +0200

    obliczenia ber

--- ZadanieIV/zadanieIV.py
+++ ZadanieIV/zadanieIV.py
@@ -18,3 +18,16 @@ noise = 0
 ch_coeff = 0
 rx_symbol = 0 
 det_symbol = 0
+for n in range (0, itr):
+    snrindB = snrindB_range[n]   
+    snr=10.0**(snrindB/10.0)
+    noise_std = 1/sqrt(2*snr)
+    noise_mean = 0
+    no_errors = 0
+    for m in range (0, N):
+      tx_symbol = 2*random.randint(0,1)-1
+      noise = random.gauss(noise_mean, noise_std)
+      rx_symbol = tx_symbol + noise
+      det_symbol = 2 * (rx_symbol >= 0) - 1
+      no_errors += 1*(tx_symbol != det_symbol)  
+      ber[n] = no_errors / N
